04e0d997ee30e89108c94ec8ea1fd1f63cbbcac8
irakli Epitashvili <irakli.epitashvili02@gmail.com>
2025-03-24 17:25:31 +0400

    modified Main in test1

--- src/main/java/TestAutomation/JavaOOP/Main.java
+++ src/main/java/TestAutomation/JavaOOP/Main.java
@@ -3,7 +3,9 @@ package TestAutomation.JavaOOP;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World2");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 
 }
